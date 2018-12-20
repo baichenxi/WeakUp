ca9b271c2c233269424385fab1cafec0e538414f
your name <your email>
2018-12-20 19:05:31 +0800

    chendebin

---
+++ app/src/main/java/com/example/m/friend.java
@@ -0,0 +1,24 @@
+package com.example.m;
+
+public class friend
+{
+    private String name;
+    private int id;
+
+    public friend(String name,int id)
+    {
+        // TODO Auto-generated constructor stub
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/m/friendadapter.java
@@ -0,0 +1,35 @@
+package com.example.m;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by lw on 2017/4/14.
+ */
+
+public class friendadapter extends ArrayAdapter{
+    private final int resourceId;
+
+    public friendadapter(Context context, int textViewResourceId, List<friend> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        friend friend = (friend) getItem(position); // 获取当前项的Fruit实例
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, null);//实例化一个对象
+
+
+
+        return view;
+    }
+}
